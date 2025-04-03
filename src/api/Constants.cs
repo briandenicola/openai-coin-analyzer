40b0b56853aba23afde7afd4bf034c5acee9848c
briandenicola <brian@briandenicola.com>
2025-04-03 08:14:34 -0500

    🎲 Thu Apr  3 08:14:34 CDT 2025

--- src/api/Constants.cs
+++ src/api/Constants.cs
@@ -20,127 +20,84 @@ public static class Constants {
     * It's okay to be incorrect or make mistakes, but be sure to provide a disclaimer that you are not a professional numismatist and that your analysis is based on the information provided.
     * Have fun with the analysis and be creative. Use your imagination to come up with interesting and unique insights about the coin.
 
-    # Common Coin Denominations:
-    ## Solidus
-    * Made of 24 carat gold. 
-    * It weighs about 4.5 grams 
-    * Brought in circulation by Constantine the Great around 310 AD.  
-    * In earlier times it was called an Aureus, which with its 8 grams was notably heavier.
-
-    ##Denarius 
-    * Made of silver. 
-    * It weighed around 4.5 grams during the time of Commodus but dwindled down to a meager 3 grams until it was abolished during Diocletian's reign. 
-    * At first it had a value of 25 against one Aureus. But around 350AD it had a value of 3.6 million against one Solidus.
-
-    ## Antonius
-    * Also made of  silver. 
-    * It is the successor of the Denarius. 
-    * Caracalla brought it into circulation in 215AD.
-    * The coin is easily recognizable because an Emperor wears a radiate crown on his head and an Emperor's shoulders are adorned with crescents.
-    * At one point it was plated in silver and was required to have a minimal content of silver, because of this it is also called a silvered Antoninus. 
-
-    ## Sestertius 
-    * Made of bronze.
-    * With its average diameter of 30 mm (in the 1st century even 35 mm) and weight of 25 grams it is the biggest of the Roman coins. 
-    * After 275 (with Aurelius as Emperor) the coin was abolished. 
-            
-    ## Dupondius 
-    * Made of brass. 
-    * It has a diameter of 27 mm and weighs 15 grams. 
-    * It had a value 1/2 a Sestertius or 2 Asses. 
-    * It is usually recognizable by it's radiate crown on the Emperor or crescents on the shoulders of the Empress. 
-    * It disappeared under the reign of Emperor Diocletian.
-
-    ## As
-    * Made of copper
-    * Has a diameter of 27 mm and weighs about 9-12 grams.  
-    * Its value was 1/4 Sestertius. 
-    * It can be differentiated from the Dupondius because its lack of radiate crown or crescents. 
-    * The As was abolished under Emperor Diocletian.
-
-    ## Follis
-    * Made of copper. 
-    * These coins were initially covered in a thin layer of silver, which mostly have disappeared over time,
-    * Originally the Follis was valued at 12.5 Denarii, later it was valued at 25 Denarii. 
-    * In 346 the coin was abolished.
-    
     # Common Emperor Abbreviations:
-        COMMON NAME:	NAME ON COINS:
-        Augustus	    C CAESAR AVG
-        Tiberius	    TI CAESAR
-        Caligula	    C CAESAR
-        Claudius	    TI CLAVD CAES
-        Nero	        NERO CLAVDIVS CAESAR
-        Galba	        SER GALBA
-        Otho	        M OTHO CAESAR
-        Vitellius	    A VITELLIVS
-        Vespasian	    CAES VESP
-        Titus	        T CAES
-        Domitian	    CAES DOMIT
-        Nerva	        NERVA CAES
-        Trajan	        NER TRAIANO
-        Hadrian	        TRAIANVS HADRIANUS
-        Antoninus Pius	ANTONINVS PIVS
-        Marcus Aurelius	AVRELIVS CAES
-        Commodus	    L AVREL COMMODVS
-        Pertinax	    P HELV PERTIN
-        Didius Julianus	    DID IVLIAN
-        Septimius Severus	L SEPT SEV
-        Geta	        P SEPT GETA
-        Caracalla	    M AVR ANTONINVS
-        Elagabalus	    M AVR ANTONINVS
-        Severus Alexander	ALEXANDER PIVS
-        Maximinus Thrax	    MAXIMINVS PIVS
-        Gordian I	        M ANT GORDIANVS
-        Gordian II	        M ANT GORDIANVS
-        Balbinus	        C D CAEL BALBINVS
-        Pupienus	        C M CLOD PVPIENVS
-        Gordian III	        M ANT GORDIANVS
-        Philip the Arab	    M IVL PHILIPPVS
-        Trajan Decius	    C M Q TRAIANVS DECIVS
-        Trebonianus Gallus	VIB TREB GALLVS
-        Aemilian	        AEMILIANVS PIVS
-        Valerian	        VALERIANVS
-        Gallienus	        GALLIENVS
-        Postumus	        C POSTVMVS
-        Victorinus	        C VICTORINVS
-        Tetricus I	        C TETRICVS
-        Tetricus II	        ESV TETRICVS
-        Claudius Gothicus	C CLAVDIVS
-        Aurelian	        C AVRELIANVS
-        Tacitus	            C M CL TACITVS
-        Florian	            C FLORIANVS
-        Probus	            C M AVR PROBVS
-        Carus	            CARVS
-        Numerian	        C NVMERIANVS
-        Carinus	            M AVR CARINVS
-        Diocletian	        DIOCLETIANVS
-        Maximianus	        C MAXIMIANVS
-        Constantius Chlorus	C CONSTANTIVS
-        Galerius	        MAXIMIANVS
-        Flavius Severus	    FL VAL SEVERVS
-        Licinius I	        C LIC LICINNIVS
-        Licinius II	        VAL LICIN LICINIVS
-        Carausius	        CARAVSIVS
-        Allectus	        C ALLECTVS
-        Constantine I	    CONSTANTINVS
-        Crispus	FL IVL      CRISPVS
-        Constantine II	    CONSTANTINVS IVN
-        Constans	        CONSTANS
-        Constantius II	    CONSTANTIVS
-        Magnentius	        CAE MAGNENTIVS
-        Constantius Gallus	CONSTANTIVS
-        Julian II	        CL IVLIANVS
-        Jovian	            IOVIANVS
-        Valentinian I	    VALENTINIANVS
-        Valens	            VALENS
-        Gratian	            GRATIANVS
-        Valentinian II	    VALENTINIANVS IVN
-        Theodosius	        THEODOSIVS
-        Magnus Maximus	    MAG MAXIMVS
-        Eugenius	        EVGENIVS
-        Arcadius	        ARCADIVS
-        Honorius	        HONORIVS
+    COMMON NAME 	    NAME ON COINS
+    ===========         ==================
+    Augustus	        C CAESAR AVG
+    Tiberius	        TI CAESAR
+    Caligula	        C CAESAR
+    Claudius	        TI CLAVD CAES
+    Nero	            NERO CLAVDIVS CAESAR
+    Galba	            SER GALBA
+    Otho	            M OTHO CAESAR
+    Vitellius	        A VITELLIVS
+    Vespasian	        CAES VESP
+    Titus	            T CAES
+    Domitian	        CAES DOMIT
+    Nerva	            NERVA CAES
+    Trajan	            NER TRAIANO
+    Hadrian	            TRAIANVS HADRIANUS
+    Antoninus Pius	    ANTONINVS PIVS
+    Marcus Aurelius	    AVRELIVS CAES
+    Commodus	        L AVREL COMMODVS
+    Pertinax	        P HELV PERTIN
+    Didius Julianus	    DID IVLIAN
+    Septimius Severus	L SEPT SEV
+    Geta	            P SEPT GETA
+    Caracalla	        M AVR ANTONINVS
+    Elagabalus	        M AVR ANTONINVS
+    Severus Alexander	ALEXANDER PIVS
+    Maximinus Thrax	    MAXIMINVS PIVS
+    Gordian I	        M ANT GORDIANVS
+    Gordian II	        M ANT GORDIANVS
+    Balbinus	        C D CAEL BALBINVS
+    Pupienus	        C M CLOD PVPIENVS
+    Gordian III	        M ANT GORDIANVS
+    Philip the Arab	    M IVL PHILIPPVS
+    Trajan Decius	    C M Q TRAIANVS DECIVS
+    Trebonianus Gallus	VIB TREB GALLVS
+    Aemilian	        AEMILIANVS PIVS
+    Valerian	        VALERIANVS
+    Gallienus	        GALLIENVS
+    Postumus	        C POSTVMVS
+    Victorinus	        C VICTORINVS
+    Tetricus I	        C TETRICVS
+    Tetricus II	        ESV TETRICVS
+    Claudius Gothicus	C CLAVDIVS
+    Aurelian	        C AVRELIANVS
+    Tacitus	            C M CL TACITVS
+    Florian	            C FLORIANVS
+    Probus	            C M AVR PROBVS
+    Carus	            CARVS
+    Numerian	        C NVMERIANVS
+    Carinus	            M AVR CARINVS
+    Diocletian	        DIOCLETIANVS
+    Maximianus	        C MAXIMIANVS
+    Constantius Chlorus	C CONSTANTIVS
+    Galerius	        MAXIMIANVS
+    Flavius Severus	    FL VAL SEVERVS
+    Licinius I	        C LIC LICINNIVS
+    Licinius II	        VAL LICIN LICINIVS
+    Carausius	        CARAVSIVS
+    Allectus	        C ALLECTVS
+    Constantine I	    CONSTANTINVS
+    Crispus	FL IVL      CRISPVS
+    Constantine II	    CONSTANTINVS IVN
+    Constans	        CONSTANS
+    Constantius II	    CONSTANTIVS
+    Magnentius	        CAE MAGNENTIVS
+    Constantius Gallus	CONSTANTIVS
+    Julian II	        CL IVLIANVS
+    Jovian	            IOVIANVS
+    Valentinian I	    VALENTINIANVS
+    Valens	            VALENS
+    Gratian	            GRATIANVS
+    Valentinian II	    VALENTINIANVS IVN
+    Theodosius	        THEODOSIVS
+    Magnus Maximus	    MAG MAXIMVS
+    Eugenius	        EVGENIVS
+    Arcadius	        ARCADIVS
+    Honorius	        HONORIVS
     ";
 
     public const string AI_PROMPT = @"I know nothing about coins, but I want to learn more about them.  Please help me understand the following coin.
