34fbf9eeca14d77a6cf49c288e512119f453b342
briandenicola <brian@briandenicola.com>
2025-04-10 13:54:53 -0500

    🏒 Thu Apr 10 13:54:53 CDT 2025

--- infrastructure/modules/aks/cluster-addons.tf
+++ infrastructure/modules/aks/cluster-addons.tf
@@ -3,7 +3,7 @@ resource "azapi_update_resource" "cluster_updates" {
     azurerm_kubernetes_cluster.this
   ]
 
-  type        = "Microsoft.ContainerService/managedClusters@2025-02-02"
+  type        = "Microsoft.ContainerService/managedClusters@2025-02-01"
   resource_id = azurerm_kubernetes_cluster.this.id
 
   body = {
