947f73aa04575909cea598b895c999f8f0857e25
briandenicola <brian@briandenicola.com>
2025-04-10 12:24:56 -0500

    Moved API Version

--- infrastructure/modules/aks/cluster-addons.tf
+++ infrastructure/modules/aks/cluster-addons.tf
@@ -3,7 +3,7 @@ resource "azapi_update_resource" "cluster_updates" {
     azurerm_kubernetes_cluster.this
   ]
 
-  type        = "Microsoft.ContainerService/managedClusters@2025-01-02-preview"
+  type        = "Microsoft.ContainerService/managedClusters@2025-02-02"
   resource_id = azurerm_kubernetes_cluster.this.id
 
   body = {
