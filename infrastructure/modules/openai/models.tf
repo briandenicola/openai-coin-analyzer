edf5bae1bc0884f88714996ff2db08dce2c6cac3
briandenicola <brian@briandenicola.com>
2025-08-12 08:04:57 -0500

    🏈 Tue Aug 12 08:04:57 CDT 2025

--- infrastructure/modules/openai/models.tf
+++ infrastructure/modules/openai/models.tf
@@ -1,10 +1,10 @@
 resource "azurerm_cognitive_deployment" "gpt" {
-  name                 = "gpt-4o"
+  name                 = "gpt-4.1"
   cognitive_account_id = azurerm_cognitive_account.this.id
   model {
     format  = "OpenAI"
-    name    = "gpt-4o"
-    version = "2024-05-13"
+    name    = "gpt-4.1"
+    version = "2025-04-14"
   }
 
   sku {
