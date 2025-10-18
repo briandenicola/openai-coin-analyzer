90ba7abcbca60d49505261191487358e9e23bf4d
briandenicola <brian@briandenicola.com>
2025-10-17 20:08:40 -0500

    🥍 Fri Oct 17 20:08:40 CDT 2025

--- src/api/Services/Reducer.cs
+++
@@ -1,6 +0,0 @@
-namespace Microsoft.SemanticKernel.ChatCompletion;
-
-public interface IChatHistoryReducer
-{
-    Task<IEnumerable<ChatMessageContent>> ReduceAsync(IReadOnlyList<ChatMessageContent> chatHistory, CancellationToken cancellationToken = default);
-}
--- src/api/Services/RicAIService.cs
+++ src/api/Services/RicAIService.cs
@@ -1,27 +1,14 @@
-#pragma warning disable SKEXP0001
 
 namespace ric.analyzer.api;
 public class RicAIService
 {
     internal static ILogger _logger;
-    internal static Kernel _kernel;
-    internal static IChatCompletionService _chat;
-    internal static OpenAIPromptExecutionSettings  _requestSettings ;
-    internal static ChatHistory _history;
-    internal static ChatHistoryTruncationReducer _reducer;
-
-    public RicAIService(ILogger<RicAIService> logger, Kernel kernel)
+    internal static AIAgent _agent;
+    
+    public RicAIService(ILogger<RicAIService> logger, AIAgent agent)
     {
-        _kernel = kernel;
+        _agent = agent;
         _logger = logger;
-
-        _chat = _kernel.GetRequiredService<IChatCompletionService>();
-        _requestSettings = new OpenAIPromptExecutionSettings(){ };
-        _reducer = new ChatHistoryTruncationReducer(targetCount: 2);
-        
-        _history = new ChatHistory();
-        _history.AddSystemMessage(Constants.SYSTEM_PROMPT);
-        _history.AddUserMessage(Constants.AI_PROMPT);
     }
     
     private static async Task<ReadOnlyMemory<byte>> ConvertFileToMemoryStream(IFormFile file)
@@ -31,35 +18,6 @@ public class RicAIService
         byte[] byteArray = memoryStream.ToArray();
         ReadOnlyMemory<byte> readOnlyMemory = new ReadOnlyMemory<byte>(byteArray);
         return readOnlyMemory;
-    }
-
-    private static async Task<ChatMessageContent> CallOpenAIEndpoint()
-    {
-        _logger.LogInformation("OpenAI API call started . . . ");
-        var chatResult = await _chat.GetChatMessageContentAsync(_history, _requestSettings, _kernel);
-        _logger.LogInformation("OpenAI API call completed . . . ");
-
-        if( chatResult.InnerContent is OpenAI.Chat.ChatCompletion chatCompletion )
-            _logger.LogInformation($"TotalTokenCount: {chatCompletion.Usage?.TotalTokenCount ?? 0} . . .");
-        
-        _logger.LogInformation($"Analyze Result: {chatResult}");
-        _logger.LogInformation($"Adding ChatResult to History . . . ");
-        _history.AddSystemMessage(chatResult.ToString());
-
-        return chatResult;
-    }
-
-    public async Task TrimHistory()
-    {
-        _logger.LogInformation("Trimming history . . . ");
-        var reducedMessages = await _reducer.ReduceAsync(_history);
-
-        if (reducedMessages is not null)
-            _history = [.. reducedMessages];
-        
-        var response = CallOpenAIEndpoint();
-        _history.AddSystemMessage(response.ToString());
-
     }
 
     public async Task<string> AnalyzeImage(IFormFile coinImage )
@@ -72,31 +30,15 @@ public class RicAIService
 
         _logger.LogInformation($"AnalyzeImage called with file: {coinImage.FileName}");
 
-        var collectionItems= new ChatMessageContentItemCollection
+        var data = new List<AIContent>
         {
             new TextContent(Constants.AI_PROMPT),
-            new ImageContent(await ConvertFileToMemoryStream(coinImage), coinImage.ContentType)
+            new DataContent(await ConvertFileToMemoryStream(coinImage), coinImage.ContentType)
         };
-        
-        _history.AddUserMessage(collectionItems);
-        _history.AddAssistantMessage("Please wait while I analyze the image...");
-        
-        return (await CallOpenAIEndpoint()).ToString();
-    }
 
-    public async Task<string> Chat(string userPrompt)
-    {
-        if (string.IsNullOrWhiteSpace(userPrompt))
-        {
-            _logger.LogError("User prompt is empty or null.");
-            throw new ArgumentException("User prompt cannot be empty or null.");
-        }
+        ChatMessage message = new(ChatRole.User, data );
 
-        _logger.LogInformation($"Chat called with prompt: {userPrompt}");
-        _history.AddUserMessage(userPrompt);
-
-        var response = await CallOpenAIEndpoint();
-        _history.AddSystemMessage(response.ToString());
+        var response = await _agent.RunAsync(message);
 
         return response.ToString();
     }
