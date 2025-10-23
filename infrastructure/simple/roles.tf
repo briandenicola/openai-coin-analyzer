8028c7c061e2ebcc4cd667e4af70414b96ebdbc4
briandenicola <brian@briandenicola.com>
2025-10-23 12:06:11 -0500

    🎲 Thu Oct 23 12:06:11 CDT 2025

--- infrastructure/simple/roles.tf
+++ infrastructure/simple/roles.tf
@@ -1,5 +1,5 @@
-resource "azurerm_role_assignment" "local_user_openai_user" {
-  scope                            = module.foundry.FOUNDRY_RESOURCE_ID
-  role_definition_name             = "Cognitive Services OpenAI User"
-  principal_id                     = data.azurerm_client_config.current.object_id
-}
+# resource "azurerm_role_assignment" "local_user_openai_user" {
+#   scope                            = module.foundry.FOUNDRY_RESOURCE_ID
+#   role_definition_name             = "Cognitive Services OpenAI User"
+#   principal_id                     = data.azurerm_client_config.current.object_id
+# }
